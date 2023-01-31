bf3127e851c2afe2998f965548011c8fee57c92d
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2023-01-31 17:58:58 +0300

    Update link active link

--- resources/views/layouts/components/nav.blade.php
+++ resources/views/layouts/components/nav.blade.php
@@ -39,7 +39,7 @@
             <div class="collapse navbar-collapse" id="navbarsExample04">
                 <ul class="navbar-nav ms-auto mb-2 mb-md-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link primary-color" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">About Us</a>
